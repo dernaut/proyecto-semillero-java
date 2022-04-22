8a982a6569a4b99814185a71fb32e5d93595a2bc
Julian Giraldo <giraldocardonajulian@gmail.com>
2022-04-22 11:11:53 -0500

    Moviendo proyecto

--- VentaLibreria2/src/main/java/com/libreria/social/interfaces/Tienda.java
+++
@@ -1,7 +0,0 @@
-package com.libreria.social.interfaces;
-
-public interface Tienda {
-	public boolean restarCantidad(int cantidad);
-	public boolean sumarCantidad(int cantidad);
-	public void listarProducto();
-}
